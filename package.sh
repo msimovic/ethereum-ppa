d56a0e290177df776ad9a5e5c53644820c524283
Marko Simovic <markobarko@gmail.com>
2014-02-15 16:45:38 +0000

    Now prompts the user to verify settings

--- package.sh
+++ package.sh
@@ -5,21 +5,41 @@ srcdir=$2
 dist=$3
 changelogmsg=$4
 srcversion=$5
+repo=$6
+
+if [ "$repo" == "" ]; then
+	repo="markobarko/ethereum"
+fi
+
+echo "Package Name: $packagename"
+echo "Source Directory: $srcdir"
+echo "Distribution: $dist"
+echo "Change Log Message: $changelogmsg"
+echo "Source Version: $srcversion"
+echo "Target PPA: $repo"
+
+echo "Are these settings correct?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) break;;
+        No ) exit;;
+    esac
+done
 
 # Prepare version information and names
 if [ "$srcversion" == "" ]; then
 	srcversion="$(date +%Y%m%d)"
 fi
-archfile="${packagename}_${srcversion}.orig.tar.bz2"
 
 PREPDIR=`mktemp -d`
 opwd=`pwd`
-
 cd $PREPDIR
 
 #prepare orig archive 
 cp -r $srcdir origsrc
 cd origsrc
+
+archfile="${packagename}_${srcversion}.orig.tar.bz2"
 tar cjf ../$archfile --exclude-vcs --exclude-backups --exclude=debian .
 cd ..
 
@@ -33,4 +53,4 @@ debchange -v "$srcversion" -p "$changelogmsg" -D "$dist"
 debuild -S -sa
 
 finalversion=`dpkg-parsechangelog | sed -n 's/^Version: //p'`
-dput -f ppa:markobarko/ethereum "../${packagename}_${finalversion}_source.changes"
+dput -f ppa:$repo "../${packagename}_${finalversion}_source.changes"
